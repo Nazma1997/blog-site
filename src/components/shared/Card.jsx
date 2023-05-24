adb6532b4300ddb3a0464d1f25c57edec769cc16
Nazma1997 <nazma.ist.707@gmail.com>
2023-05-24 10:49:53 +0600

    card

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -10,15 +10,15 @@ const Card = ({item}) => {
   return (
     <div className="	 rounded-lg shadow-2xl overflow-hidden max-w-sm mx-auto animate-rotate pb-16 my-6 ">
       <img src={item?.image} alt="Card" className="w-full h-5/6 object-cover card bg-white rounded-lg" />
-      <img src={author} alt='the author' className="-mt-16 ml-8  w-1/6 rounded-full  author" />
+      <img src={author} alt='the author' className="-mt-16 ml-8  w-1/6 rounded-full  author2" />
       <div className="px-4  mt-8">
         <div className='flex'>
-        <Link to='/design' className='bg-gray-500 text-white w-4/12 rounded-2xl text-center py-1 text-bold mb-2'><p >Design</p></Link>
-       <Link to='/technology' className='bg-gray-500 text-white w-4/12 rounded-2xl text-center py-1 text-bold mb-2 mx-1'> <p >Technology</p></Link>
-        <Link to='/product' className='bg-gray-500 text-white w-4/12 rounded-2xl text-center py-1 text-bold mb-2'><p >Product</p></Link>
+        <Link to='/design' className='bg-gray-500 text-white w-4/12 rounded-2xl text-center py-1 text-bold mb-2 '><p className='hover:underline' >Design</p></Link>
+       <Link to='/technology' className='bg-gray-500 text-white w-4/12 rounded-2xl text-center py-1 text-bold mb-2 mx-1'> <p className='hover:underline'>Technology</p></Link>
+        <Link to='/product' className='bg-gray-500 text-white w-4/12 rounded-2xl text-center py-1 text-bold mb-2'><p className='hover:underline' >Product</p></Link>
         </div>
-        <h3 className="text-xl text-white  font-bold mb-2"> {item?.title}</h3>
-        <h3 className="text-white font-normal text-xl ">{item?.shortDescription}</h3>
+        <h3 className="text-xl text-white  font-bold mb-2 hover:underline"> {item?.title}</h3>
+        <h3 className="text-white font-normal text-xl hover:underline ">{item?.shortDescription}</h3>
        
       </div>
     </div>
