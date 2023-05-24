86f837f9db611330709ab6594859beb5e325c875
Nazma1997 <nazma.ist.707@gmail.com>
2023-05-24 11:25:09 +0600

    card

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import logo from '../images/logo.png';
 import '../style.css';
 import Card from './shared/Card';
+import { useGetPostQuery } from '../redux/apiSlice/userSlice';
 
 const Author = () => {
+  const { data: posts } = useGetPostQuery();
+
   return (
    <div className='pb-10'>
      <div className=' lg:flex md:flex justify-between pb-10'>
@@ -21,12 +24,14 @@ const Author = () => {
       
       
     </div>
-     <div className="lg:flex">
-     <Card />
-     <Card />
-     <Card />
-     <Card />
-     </div>
+   
+    <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
+         {
+          posts?.map(item =>
+             <Card  item={item} key={item._id}/>
+            )
+         }
+      </div>
      <div className='flex justify-center'>
      <p className=' px-5 py-4 rounded-lg text-xl font-semibold text-white  bg-gray-800 mb-5'>See More</p>
      </div>
--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Card from './shared/Card'
 import Subscribe from './Subscribe'
+import { useGetPostQuery } from '../redux/apiSlice/userSlice';
 
 const Recommended = () => {
+  const { data: posts } = useGetPostQuery();
   return (
     <div>
       <h1 className='text-white  text-4xl py-5'>Recommended</h1>
-      <div className="lg:flex">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        </div>
+     
+    <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
+         {
+          posts?.map(item =>
+             <Card  item={item} key={item._id}/>
+            )
+         }
+      </div>
      <Subscribe />
         <hr />
     </div>
