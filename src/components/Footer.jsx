05a2bc7231844dc25cd03cac8f7672ed77015fac
Nazma1997 <nazma.ist.707@gmail.com>
2023-05-26 15:28:49 +0600

    fix profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../images/logo.png';
 import { MdFacebook } from 'react-icons/md';
+import { AiFillLinkedin, AiFillTwitterCircle, AiFillYoutube } from 'react-icons/ai';
 import '../style.css'
 
 const Footer = () => {
@@ -9,91 +10,31 @@ const Footer = () => {
       <div className='lg:flex justify-evenly mt-10 pb-10'>
       <div className='lg:w-4/12'>
         <img src={logo} className='logo'/>
-        <p className='text-white text-2xl '>A minimal, functional theme for running a paid-membership publication on Ghost.</p>
+        <p className='text-white text-2xl '>We are a community that celebrates contemporary culture
+focused on technology, design, art and web.</p>
       </div>
      <div className='md:flex'>
-     <div className=''>
-          <div className='flex'>
-          <div>
-            <p className='text-white text-xl '> Social</p>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-          </div>
-          <div className='ml-12'>
-            <p className='text-white text-xl '> About</p>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-          </div>
-
-          </div>
-
-      </div>
+     
       <div className=''>
-          <div className='flex'>
-          <div>
-            <p className='text-white text-xl '> Social</p>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-            <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
-            </div>
-          </div>
+          <div className='flex mt-5'>
+         
           <div className='ml-12'>
-            <p className='text-white text-xl '> Social</p>
+            <p className='text-white text-xl '> Contract</p>
             <div className='flex mt-5'>
                <MdFacebook className='text-white text-2xl'/>
                <p className='text-white ml-2'> Facebook</p>
             </div>
             <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
+               <AiFillTwitterCircle className='text-white text-2xl'/>
+               <p className='text-white ml-2'> Twitter</p>
             </div>
             <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
+               <AiFillLinkedin className='text-white text-2xl'/>
+               <p className='text-white ml-2'> Linkedin</p>
             </div>
             <div className='flex mt-5'>
-               <MdFacebook className='text-white text-2xl'/>
-               <p className='text-white ml-2'> Facebook</p>
+               <AiFillYoutube className='text-white text-2xl'/>
+               <p className='text-white ml-2'> Youtube</p>
             </div>
           </div>
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useGetPostQuery } from '../redux/apiSlice/userSlice';
 import Card from './shared/Card';
 import Navbar from './shared/Navigation';
@@ -10,24 +10,113 @@ const Profile = () => {
   const email = localStorage.getItem('email');
   const filteredItems = posts?.filter(item => item.email === email);
 
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+
+
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+
+    if (value === '') {
+      setSearchResults([]);
+    } else {
+      const results = filteredItems.filter((item) =>
+        item.title.toLowerCase().includes(value.toLowerCase())
+      );
+      setSearchResults(results);
+    }
+  };
+
+  // Pagination
+  const itemsPerPage = 12;
+  const [currentPage, setCurrentPage] = useState(1);
+
+
+  const totalItems = filteredItems?.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  // Function to handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedItems = filteredItems?.slice(startIndex, endIndex);
+
+
   return (
   <>
    <Navbar />
-   <div>
-      <h1 className='text-white  text-4xl py-5'><strong>All Posts</strong></h1>
-      {/* <div className="lg:flex">
-        <Card />
-      
-        </div> */}
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
-         {
-          filteredItems?.map(item =>
-             <Card  item={item} key={item._id}/>
+   <div className='flex justify-center'>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearch}
+          className='bg-slate-800 lg:w-6/12 w-8/12 text-2xl h-20 text-center text-white rounded-2xl mt-16 hover:outline-none border-0 shadow-2xl'
+        />
+      </div>
+      <div className='pb-10'>
+        <h1 className='text-white  text-4xl py-5'> <strong>Your profile</strong></h1>
+
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-4">
+
+
+          {searchTerm === ''
+
+            ? displayedItems?.map(item =>
+              <Card item={item} key={item._id} />
             )
-         }
+            : searchResults.map((item) => (
+              <Card item={item} key={item._id} />
+            ))}
+        </div>
+
+      </div>
+
+
+      <div className='flex justify-center mb-10'>
+        <button
+          disabled={currentPage === 1}
+          onClick={handlePreviousPage}
+          className='bg-slate-800 text-white px-10 py-3 rounded-2xl shadow-lg text-xl font-bold'
+        >
+          Previous
+        </button>
+
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageChange(index + 1)}
+            className={currentPage === index + 1 ? 'active' : ''}
+            className='text-2xl mx-5 text-white font-bold'
+            
+          >
+            {index + 1}
+          </button>
+        ))}
+
+        <button
+          disabled={currentPage === totalPages}
+          onClick={handleNextPage}
+          className='bg-slate-800 text-white px-10 py-3 rounded-2xl shadow-lg text-xl font-bold'
+        >
+          Next
+        </button>
       </div>
       <hr />
-    </div>
     <Footer />
   </>
   )
